0099c23180f3c7b7faa21b469ccf98c176648c5d
Osman Onur BAŞ <onurbass.ob@gmail.com>
2023-07-27 11:16:12 +0300

    27072023

---
+++ SpringIntro/src/springIntro/AppMain.java
@@ -0,0 +1,11 @@
+package springIntro;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.add();
+
+	}
+
+}
---
+++ SpringIntro/src/springIntro/CustomerDal.java
@@ -0,0 +1,10 @@
+package springIntro;
+
+public class CustomerDal {
+
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("Oracle veri tabanına eklendi");
+	}
+
+}
