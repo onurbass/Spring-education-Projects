bd02fc75b0e720f20c0ab6524e2c22e0d28f9544
Osman Onur BAŞ <onurbass.ob@gmail.com>
2023-08-12 23:16:09 +0300

    bean deneme

--- SpringTeory/OsmanOnurBAŞ-SpringBootProje/src/main/java/com/onurbas/SpringBootMonoliticRestApiApplication.java
+++ SpringTeory/OsmanOnurBAŞ-SpringBootProje/src/main/java/com/onurbas/SpringBootMonoliticRestApiApplication.java
@@ -2,12 +2,19 @@ package com.onurbas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootMonoliticRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMonoliticRestApiApplication.class, args);
+
+		ApplicationContext applicationContext=SpringApplication.run(SpringBootMonoliticRestApiApplication.class,args);
+	  for (String s : applicationContext.getBeanDefinitionNames()
+		   ) {
+		System.out.println(s);
+	  }
+
 	}
 
 }
