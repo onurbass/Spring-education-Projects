874e82178464cc0fe6dfa094f6c8face93fd8f94
onurbass <onurbass.ob@gmail.com>
2023-09-10 22:12:05 +0300

    all service to kubernetes

--- SpringPractice/Java10SocialMedia-master/auth-service/src/main/java/com/bilgeadam/manager/IUserManager.java
+++ SpringPractice/Java10SocialMedia-master/auth-service/src/main/java/com/bilgeadam/manager/IUserManager.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.bilgeadam.constant.EndPoints.*;
 
-@FeignClient(url = "${feign.user}",decode404 = true,name = "auth-userprofile")
+@FeignClient(url = "${feign.user}", decode404 = true, name = "auth-userprofile")
 public interface IUserManager {
 
-    @PostMapping(SAVE)
-   ResponseEntity<Boolean> save(@RequestBody UserSaveRequestDto dto,@RequestHeader("Authorization") String token);
+  @PostMapping(SAVE)
+  ResponseEntity<Boolean> save(@RequestBody UserSaveRequestDto dto,@RequestHeader("Authorization") String token);
 
-    @PostMapping(ACTIVATE_STATUS)
-    ResponseEntity<String> activateStatus(@RequestParam String token);
+  @PostMapping(ACTIVATE_STATUS)
+  ResponseEntity<String> activateStatus(@RequestParam String token);
 
-    @DeleteMapping(DELETE_BY_ID)
-    public ResponseEntity<String> deleteById(@RequestHeader("Authorization") String token);
+  @DeleteMapping(DELETE_BY_ID)
+  public ResponseEntity<String> deleteById(@RequestHeader("Authorization") String token);
 
 }
